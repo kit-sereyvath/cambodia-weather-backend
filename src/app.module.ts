de155018999327e4a2b8515aaa2cfccdaa71c70c
Kim Sereyvath <ksereyvath@outlook.com>
2022-02-13 00:02:58 +0700

    fix/ first publish 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { WeatherDataModule } from './weather-data/weather-data.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
         const isProduction = configService.get('STAGE') === 'prod';
-        console.log(isProduction)
+        console.log(configService.get('STAGE'))
         return {
           ssl: isProduction,
           extra: {
